ede4545b8f0f731896e8a2f4dc5639a78ff2353a
Oleg.Stelm <oleg.stelm@gmail.com>
2022-12-02 00:09:13 +0300

    InitCommitAddProj005

---
+++ Programm005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter user name ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Masha")
+{
+    Console.WriteLine("Hooray! It's MASHA!");
+}
+else
+{
+    Console.Write("Wazzup, ");
+    Console.Write(username);
+    Console.WriteLine("!");
+}
