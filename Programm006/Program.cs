dae70f459893140129428972fb896aa1b51a7bfa
Oleg.Stelm <oleg.stelm@gmail.com>
2022-12-02 00:14:57 +0300

    InitCommitAddProj006

---
+++ Programm006/Program.cs
@@ -0,0 +1,15 @@
+﻿int a=232;
+int b=544;
+int c=756;
+int d=134;
+int e=168;
+
+int max=a;
+if(a>max)max=a;
+if(b>max)max=b;
+if(c>max)max=c;
+if(d>max)max=d;
+if(e>max)max=e;
+
+Console.Write("max=");
+Console.WriteLine(max);
